15bf98b19d06aaf338a5824723defc31f5b29872
CodeWithVirag <codewithvirag@gmail.com>
2022-06-28 13:59:53 +0530

    Header and Footer Includes Setup

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require APPROOT . '/views/inc/header.php'; ?>
     <h1><?php echo $data['title']; ?></h1>
-    
-</body>
-</html>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
