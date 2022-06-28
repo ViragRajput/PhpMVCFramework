e3c266c150d97ca625ccdecc8b03ff70a91a0fd8
CodeWithVirag <codewithvirag@gmail.com>
2022-06-28 18:17:07 +0530

    Add Database PDO Classes and Prepared Stmt

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,12 +2,15 @@
 
 class Pages extends Controller {
     public function __construct() {
-        
+        $this->postModel = $this->model('Post');
     }
 
     public function index() {
+        $posts = $this->postModel->getPosts();
+
         $data = [
-            'title' => 'Welcome message'
+            'title' => 'Welcome message',
+            'posts' => $posts
         ];
 
         $this->view('pages/index', $data);
